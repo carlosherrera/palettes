abb24f4395b533468e33a9aa054f26fc0ab2c205
Carlos Herrera <carlosherreramty@gmail.com>
2016-11-21 17:21:42 -0600

    Initial checkin. Just adding main files.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="css/animate.css">
+<script type="text/javascript" src="js/site.js"></script>
+</head>
+
+<body>
+<h2>Pallettes</h2>
+
+<p>
+<label for="filename">Image: </label>
+<input type="file" id="filename" />
+<input type="button" value="Process"/>
+</p>
+
+<p>
+<img id="selectedImg" />
+<div id="palletter"></div>
+</p>
+
+</body>
+
+</html>
